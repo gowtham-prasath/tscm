0888c5e836e0331a2b825c07422e021acfce29ea
Gowtham Prasath <Gowtham.Prasath@Incture.local>
2019-11-13 10:49:58 +0530

    nan tha

---
+++ eklavyaduplicate/src/main/java/com/incture/eklavyaduplicate/App.java
@@ -0,0 +1,15 @@
+package com.incture.eklavyaduplicate;
+import com.incture.eklavya.Mavenprojectclass1;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Mavenprojectclass1.print();
+    }
+    
+}
---
+++ eklavyaduplicate/src/main/java/com/incture/eklavyaduplicate/Mavenduplicateprojectclass.java
@@ -0,0 +1,12 @@
+package com.incture.eklavyaduplicate;
+
+public class Mavenduplicateprojectclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Maven duplicate");
+	}
+public void printdup(){
+	System.out.println("eklavyadup");
+}
+}
